a5af42c6d84322370cb946fa6b55efdbbc14c5bf
LuisAngelNV <89865076+LuisAngelNV@users.noreply.github.com>
2023-03-01 17:16:17 -0600

    Se creo un mensaje de prueba medienate gitHub Page

--- src/Forms/Formulario.java
+++ src/Forms/Formulario.java
@@ -101,6 +101,10 @@ public class Formulario extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
+        // Creacion de mensaje muestra
+        Sistem.out.println("Mensaje de saludo");
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
